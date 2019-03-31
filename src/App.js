d05e11f5d538f419171cb950b62c84ae3ef5466c
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2019-03-31 12:28:43 +0700

    update package json

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1>welcome to uji yuksinau, tempat sinau paling oke</h1>
+        <h1>Welcome to uji yuksinau, tempat sinau paling oke</h1>
       </div>
     )
   }
