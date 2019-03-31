bfa92a9da143e8256508badcca6d523b49022dba
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2019-03-31 13:08:16 +0700

    configure redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component} from "react"
 import {hot} from "react-hot-loader"
+import { connect } from 'react-redux'
+
+import { simpleAction } from './actions/simpleAction';
+
 import "./App.css"
 
 class App extends Component{
+
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
   render(){
     return(
       <div className="App">
-        <h1>Welcome to uji yuksinau, tempat sinau paling oke</h1>
+        <h1>Welcome to uji yuksinau, tempat sinau paling oke!</h1>
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
       </div>
     )
   }
 }
 
-export default hot(module)(App)
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = (dispatch )=> ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(hot(module)(App))
