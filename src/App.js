eb6298e47a47fccbc1038874a42532e3967a6f1d
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2019-03-31 11:09:46 +0700

    configure react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react"
+import {hot} from "react-hot-loader"
+import "./App.css"
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1>welcome to uji yuksinau, tempat sinau paling oke</h1>
+      </div>
+    )
+  }
+}
+
+export default hot(module)(App)
