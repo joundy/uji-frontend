0a21859ebf5e6152d41079517e06d81c3134e5a3
haffjjj <hafizjoundys@gmail.com>
2019-05-01 13:45:10 +0700

    update docker

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express = require("express")
-const path = require("path")
+const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.disable("x-powered-by")
-app.use(express.static(path.join(__dirname, "/build")))
+app.use(express.static(path.join(__dirname, 'build')))
 
-app.get("*", (res) =>{
-    res.sendFile(path.join(__dirname+"/build/index.html"))
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/build/index.html'))
 })
 
-const port = process.env.PORT || 5000
+const port = 5000
 app.listen(port)
 
-console.log("App is listening on port " + port)
+console.log('App is listening on port ' + port)
